6d1fd65b2116acd15c85c6045e40f7f744ed6c59
Gonzalo Gamez <takosaga@gmail.com>
2021-12-28 13:57:35 +0200

    using ganache for local transactions

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -2,6 +2,10 @@ from semantic_version.base import SimpleSpec
 from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -34,9 +38,50 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for connecting to ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 5777
+chain_id = 1337
 my_address = "0x5ECda938Fa45a4bD95C352808c4E696932095a21"
-private_key = "0x23b09b08d1dca8093354933628be0548b0acd03a1dd9e0720840fc1b51422f8e"
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latesst transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# Get gas price
+gas_price = w3.eth.gas_price
+# 1. Build transaction
+# 2. Sign transaction
+# 3. Send transaction
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "gasPrice": gas_price, "from": my_address, "nonce": nonce}
+)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# Send sign transaction
+print("Deploying contract...")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed")
+# Working with the contract
+# Contract Address
+# Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call ->  Simulate making the call and getting a return a value
+# Transact -> Actually make a state change
+
+# Initial value of favorite number
+print(simple_storage.functions.retrieve().call())
+print("Updating Contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "gasPrice": gas_price, "from": my_address, "nonce": nonce + 1}
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated")
+print(simple_storage.functions.retrieve().call())
