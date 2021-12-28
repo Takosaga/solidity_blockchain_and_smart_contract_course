ceb404241932546794499db1b42004b66fc15d25
Gonzalo Gamez <takosaga@gmail.com>
2021-12-28 02:38:25 +0200

    added json along with starting with deploying on ganache

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,2 +1,42 @@
+from semantic_version.base import SimpleSpec
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
+
+# Compile Our Solidty
+install_solc("0.6.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("comiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0x5ECda938Fa45a4bD95C352808c4E696932095a21"
+private_key = "0x23b09b08d1dca8093354933628be0548b0acd03a1dd9e0720840fc1b51422f8e"
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
