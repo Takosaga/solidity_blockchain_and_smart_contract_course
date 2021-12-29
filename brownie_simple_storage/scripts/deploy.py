f2c1970a1036481b65590feda28770aecb01155c
Gonzalo Gamez <takosaga@gmail.com>
2021-12-29 17:54:17 +0200

    End of Lesson 5

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     # print(account)
     # account = accounts.add(config["wallets"]["from_key"])
     # print(account)
@@ -13,6 +13,13 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
