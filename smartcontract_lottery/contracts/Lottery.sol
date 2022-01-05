1d73046d1a0505c29536fd0dc2d148eef26cfe18
Gonzalo Gamez <takosaga@gmail.com>
2022-01-05 02:32:18 +0200

    unit test written and passed

--- smartcontract_lottery/contracts/Lottery.sol
+++ smartcontract_lottery/contracts/Lottery.sol
@@ -19,6 +19,7 @@ contract Lottery is VRFConsumerBase, Ownable {
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
     bytes32 public keyhash;
+    event RequestedRandomness(bytes32 requestId);
 
     constructor(
         address _priceFeedAddress,
@@ -57,8 +58,19 @@ contract Lottery is VRFConsumerBase, Ownable {
     }
 
     function endLottery() public onlyOwner {
+        // uint256(
+        //     keccack256(
+        //         abi.encodePacked(
+        //             nonce, // nonce is preditable (aka, transaction number)
+        //             msg.sender, // msg.sender is predictable
+        //             block.difficulty, // can actually be manipulated by the miners!
+        //             block.timestamp // timestamp is predictable
+        //         )
+        //     )
+        // ) % players.length;
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
-        bytes32 requestID = requestRandomness(keyhash, fee);
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        emit RequestedRandomness(requestId);
     }
 
     function fulfillRandomness(bytes32 _requestID, uint256 _randomness)
--- smartcontract_lottery/scripts/helpful_scripts.py
+++ smartcontract_lottery/scripts/helpful_scripts.py
@@ -6,6 +6,7 @@ from brownie import (
     Contract,
     LinkToken,
     VRFCoordinatorMock,
+    interface,
 )
 from web3 import Web3
 
@@ -36,11 +37,10 @@ contract_to_mock = {
 
 def get_contract(contract_name):
     """This function will grab the contract addresses from the brownie config
-    if defined, otherwise it will deploy a mock version of that contract, and return that mock contract.
-
+    if defined, otherwise, it will deploy a mock version of that contract, and
+    return that mock contract.
         Args:
             contract_name (string)
-
         Returns:
             brownie.network.contract.ProjectContract: The most recently deployed
             version of this contract.
@@ -48,18 +48,23 @@ def get_contract(contract_name):
     contract_type = contract_to_mock[contract_name]
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         if len(contract_type) <= 0:
+            # MockV3Aggregator.length
             deploy_mocks()
         contract = contract_type[-1]
+        # MockV3Aggregator[-1]
     else:
         contract_address = config["networks"][network.show_active()][contract_name]
+        # address
+        # ABI
         contract = Contract.from_abi(
             contract_type._name, contract_address, contract_type.abi
         )
+        # MockV3Aggregator.abi
     return contract
 
 
 DECIMALS = 8
-INITIAL_VALUE = 200000000000
+INITIAL_VALUE = 200_000_000_000
 
 
 def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
@@ -68,3 +73,16 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     link_token = LinkToken.deploy({"from": account})
     VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Mock Deployed.")
+
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, ammount=100_000_000_000_000_000
+):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, ammount, {"from": account})
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    # tx = link_token_contract.transfer(contract_address, ammount, {"from": account})
+    tx.wait(1)
+    print("Fund contract!")
+    return tx
